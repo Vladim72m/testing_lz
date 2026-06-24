670385215ddc183cd27964162073cba3a0f04a29
Vladim72m <vladim72dx@tutamail.com>
2026-06-24 13:39:53 +0300

    Update equation.py

--- equation.py
+++ equation.py
@@ -4,3 +4,8 @@ def equation(a: int, b: int, c: int, d: int) -> float:
     if c == d:
         return "Ошибка: деление на ноль"
     return ((a - b) ** 0.5) / (c - d)
+
+if __name__ == "__main__":
+    print("Обычный расчет:", equation(9, 5, 5, 3))  
+    print("Ошибка корня:", equation(3, 5, 10, 2))  
+    print("Деление на ноль:", equation(10, 1, 5, 5)) 
